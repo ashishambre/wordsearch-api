5f3e2d874b1f2212c16097dc0dfaa8aa1a5e4b4e
Aashish Ambre <a.ambre06@gmail.com>
2020-05-18 12:46:52 +0530

    WordSearch API Implementation

---
+++ src/main/java/com/ashishambre/wordsearchapi/WordsearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.ashishambre.wordsearchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordsearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordsearchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashishambre/wordsearchapi/WordsearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashishambre.wordsearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordsearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
